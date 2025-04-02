239b9c03853770913689fe77dcae4bb7e0ccae73
Yossi Frances <yfrans@gmail.com>
2025-04-02 08:46:10 +0300

    added missing config params

--- kong/plugins/oidc/utils.lua
+++ kong/plugins/oidc/utils.lua
@@ -63,6 +63,8 @@ function M.get_options(config, ngx)
     filters = parseFilters(config.filters),
     logout_path = config.logout_path,
     redirect_after_logout_uri = config.redirect_after_logout_uri,
+    websocket_auth = config.websocket_auth,
+    websocket_token_param = config.websocket_token_param,
   }
 end
 
